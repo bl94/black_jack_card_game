31bd55f974f4e2b5eab39ee9c81de7abc88a0665
bl94 <bartekazki@gmail.com>
2021-10-14 13:21:41 +0200

    code after correction from pylint

--- black_jack_game.py
+++ black_jack_game.py
@@ -1,12 +1,19 @@
+"""
+BLACKJACK CARD GAME
+"""
 from random import shuffle
-import os
+
 #properties of cards
-color=("Spades","Hearts","Clubs","Diamonds")
+colors=("Spades","Hearts","Clubs","Diamonds")
 figures=("Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King","Ace")
 cards_and_values={'Two':2,"Three":3,"Four":4,"Five":5,"Six":6,"Seven":7,"Eight":8,"Nine":9,\
 "Ten":10,"Jack":10,"Queen":10,"King":10,"Ace":11}
 
+
 class Card():
+    """
+    CLASS CARD
+    """
     def __init__(self,color_card,figure):
         self.color_card=color_card
         self.figure=figure
@@ -21,6 +28,9 @@ class Card():
         return self.__str__()
 
 class Deck():
+    """
+    CLASS DECK
+    """
     def __init__(self):
         self.all_of_cards =[]
 
@@ -28,40 +38,54 @@ class Deck():
     def __len__(self):
         return len(self.all_of_cards)
 
-    #create deck
     def create_deck_cards(self):
-        for x in color :
-            for y in figures:
-                new_card=Card(x,y)
+        """
+        create deck
+        """
+        for card_color in colors :
+            for card_figure in figures:
+                new_card=Card(card_color,card_figure)
                 self.all_of_cards.append(new_card)
 
-    #shuffle deck
     def shuffle(self):
+        """
+        shuffle deck
+        """
         shuffle(self.all_of_cards)
 
-    #remove card from the top of the deck
     def remove_cards(self):
+        """
+        remove card from the top of the deck
+        """
         self.all_of_cards.pop(0)
 
 class Player():
+    """
+    CLASS DECK
+    """
     def __init__(self):
         self.player_cards=[]
         self.value_of_player_cards=0
 
-    #how many cards are in the player's hand
     def __len__(self):
+        """
+        how many cards are in the player's hand
+        """
         return len(self.player_cards)
 
-    #deal card to player
-    #and removce card from the deck
     def add_card(self,card,deck):
-        self.player_cards.append(card)    
+        """
+        deal card from the deck and remove this card from top in the deck
+        """
+        self.player_cards.append(card)
         self.value_of_player_cards+=card.value
         deck.remove_cards()
 
-    #method HIT or STAY, if player selects HIT, he will draw a card from the deck
-    #if he selects STAY, he will stay with these cards that we have in hand
     def hit_or_stay(self,card,deck):
+        """
+        method HIT or STAY, if player selects HIT, he will draw a card from the deck
+        if he selects STAY, he will stay with these cards that we have in hand
+        """
         print("Player Cards: ")
         print(self.player_cards)
         loop=True
@@ -90,27 +114,39 @@ class Player():
     #if player has got ace/s and value of cards in hand exceed 21
     #reduce value of player's cards by 10
     def final_checking_cards(self):
-        cards_list=[x.figure for x in self.player_cards if x.figure=="Ace"]
-        x=0
-        while x<=cards_list.count("Ace") and self.value_of_player_cards>21:
+        """
+        check has player got aces in hand
+        if player has got ace/s and value of cards in hand exceed 21
+        reduce value of player's cards by 10
+        """
+        cards_list=[card.figure for card in self.player_cards if card.figure=="Ace"]
+        ace_count=0
+        while ace_count<=cards_list.count("Ace") and self.value_of_player_cards>21:
             self.value_of_player_cards-=10
-            x+=1
+            ace_count+=1
 
 class Dealer():
+    """
+    CLASS DEALER
+    """
     def __init__(self):
         self.dealer_cards=[]
         self.value_of_dealer_cards=0
 
-    #deal card to player
     def add_card(self,card,deck):
+        """
+        deal card to player
+        """
         self.dealer_cards.append(card)
         self.case_of_ace_in_dealer_cards()
         self.value_of_dealer_cards+=card.value
         deck.remove_cards()
 
-    #check has dealer got aces in hand
-    #if he has got more aces than one and we reduce value of dealer cards by 10
     def case_of_ace_in_dealer_cards(self):
+        """
+        check has dealer got aces in hand
+        if he has got more aces than one and we reduce value of dealer cards by 10
+        """
         cards_list=[x.figure for x in self.dealer_cards]
         how_many_aces_in_dealer_cards=cards_list.count("Ace")
         if how_many_aces_in_dealer_cards>1:
@@ -119,19 +155,32 @@ class Dealer():
     #check Do value of dealer's cards exceed 16
     #if value 0f dealer's cards is lower or equal to 16, dealer draw a card
     def checking_cards(self,card,deck):
+        """
+        check Do value of dealer's cards exceed 16
+        if value 0f dealer's cards is lower or equal to 16, dealer draw a card
+        """
         while self.value_of_dealer_cards <= 16:
             print("Dealer hit")
             print("")
             self.add_card(card,deck)
 
 class GameLogic():
-
+    """
+    CLASS GAME LOGIC
+    """
     #check who win
     #if value of player's or dealer's cards exceed 21, they will lose
     #if value of player's cards is closer 21 than value of dealer's cards, player win
     #and exactly the same in backward
     #if value of player's cards is equal to value of dealer's cards is draw
-    def who_win(player,dealer):
+    def who_win(self,player,dealer):
+        """
+        check who win
+        if value of player's or dealer's cards exceed 21, they will lose
+        if value of player's cards is closer 21 than value of dealer's cards, player win
+        and exactly the same in backward
+        if value of player's cards is equal to value of dealer's cards is draw
+        """
         if player.value_of_player_cards > 21:
             print("Buzz. Value of your cards exceed 21. Dealer win")
         elif dealer.value_of_dealer_cards > 21:
@@ -144,8 +193,10 @@ class GameLogic():
             else:
                 print("Draw")
 
-    #check does player wants to play again
-    def play_again():
+    def play_again(self):
+        """
+        check does player wants to play again
+        """
         play_or_not_play=""
         while play_or_not_play not in ["Y","N"]:
             play_or_not_play=input("Do you want play again? Y or N : ")
@@ -158,6 +209,9 @@ class GameLogic():
                 print("You enter unfamiliar character")
 
 def main():
+    """
+    main function
+    """
     game_on=True
     while game_on:
         my_deck = Deck()
@@ -168,23 +222,23 @@ def main():
         #create player and dealer
         player = Player()
         dealer = Dealer()
-        x=0
+        hand=0
         #loop in which we deal cards
-        while x<2:
-            print(f"{x+1} hand")
+        while hand<2:
+            print(f"{hand+1} hand")
             player.add_card(my_deck.all_of_cards[0],my_deck)
             print("Player Cards: ")
             print(player.player_cards)
             dealer.add_card(my_deck.all_of_cards[0],my_deck)
             #player see only first dealer's card but after the second hand
-            if x==1:
+            if hand==1:
                 print("")
                 print("Dealer card:")
                 print(dealer.dealer_cards[0])
                 print('')
-            x+=1
+            hand+=1
         is_hit_or_stay=True
-        #loop in which there is method HIT or STAY,if player selects HIT, 
+        #loop in which there is method HIT or STAY,if player selects HIT
         #he will draw a card from the deck
         #if he selects STAY,he will stay with these cards that we have in hand
         while is_hit_or_stay:
@@ -198,14 +252,10 @@ def main():
         player.final_checking_cards()
         print("")
         #Game Logic check who win
-        GameLogic.who_win(player,dealer)
+        game_logic=GameLogic()
+        game_logic.who_win(player,dealer)
         print("")
         #Game Logic check Does player want to play again?
-        game_on=GameLogic.play_again()
+        game_on=game_logic.play_again()
 
-#main()
-old_name = r"C:\Users\Bartlomiej Lepa\Desktop\Projects\BlackJackGame\BlackJackGame.py"
-new_name = r"C:\Users\Bartlomiej Lepa\Desktop\Projects\BlackJackGame\black_jack_game.py"
-
-# Renaming the file
-os.rename(old_name, new_name)
+main()
